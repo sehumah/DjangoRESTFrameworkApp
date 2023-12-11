f56083fd569a56e868ddcbaae08e354d6e6787f6
Stephen Ehumah <sehumah@protonmail.com>
2023-12-11 16:30:15 +0000

    add view functionality to update a drink entry in the db & include the view in the urls py file

--- drink/urls.py
+++ drink/urls.py
@@ -4,4 +4,5 @@ from drink import views
 
 urlpatterns = [
     path('drinks/', views.drinks, name='drinks'),
+    path('drinks/<int:drink_id>', views.detail, name='detail')
 ]
--- drink/views.py
+++ drink/views.py
@@ -23,3 +23,30 @@ def drinks(request):
         if drink_serializer.is_valid():
             drink_serializer.save()
             return Response(drink_serializer.data, status=status.HTTP_201_CREATED)
+
+# the Response() isn't JSON specific which allows us to see an HTML view of the page where
+# we can work with the data through the web page, similar to the admin panel
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def detail(request, drink_id):
+    # get the drink from the DB using its id. check to see if the given id is valid
+    try:
+        drink = Drink.objects.get(pk=drink_id)
+    except Drink.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        drink_serializer = DrinkSerializer(drink)
+        return Response(data=drink_serializer.data)
+
+    elif request.method == 'PUT':
+        drink_serializer = DrinkSerializer(drink, data=request.data)
+        if drink_serializer.is_valid():
+            drink_serializer.save()
+            return Response(drink_serializer.data)  # give that data back
+        else:
+            return Response(drink_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        pass
