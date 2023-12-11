337234a63332d9661722e1ea0f5110884dd1e6c1
Stephen Ehumah <sehumah@protonmail.com>
2023-12-11 14:49:35 +0000

    change model field 'description' to a TextField instead of a CharField and make and apply migrations to the drink app

--- drink/models.py
+++ drink/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Drink(models.Model):
     name = models.CharField(max_length=50)
-    description = models.CharField(max_length=500)
+    description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
