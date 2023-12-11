e100054c0991eccfba07814448fe27162569f8b8
Stephen Ehumah <sehumah@protonmail.com>
2023-12-11 15:15:44 +0000

    throw drink_serializer.data into a dictionary to get an object instead of a list

--- drink/views.py
+++ drink/views.py
@@ -10,4 +10,4 @@ def drinks(request):
     # get all the drinks, serialize them & return them in json
     drinks = Drink.objects.all()
     drink_serializer = DrinkSerializer(drinks, many=True)
-    return JsonResponse(drink_serializer.data, safe=False)
+    return JsonResponse({'drinks': drink_serializer.data}, safe=False)  # instead of a list if you want to have an object, throw the serializer.data into a dictionary
